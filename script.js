67ddfd818eaece6b52058fec75301ca6d9a98e73
Otabek <25@.ru>
2021-12-16 00:06:45 +0500

    JS-1

---
+++ script.js
@@ -0,0 +1,16 @@
+var str = prompt('Введите своё имя')
+console.log('Мое имя ' + str);
+var str = +prompt('Введите свой возраст')
+console.log('Мне ' + str);
+var str = prompt('Решите пример 6+3=?')
+console.log('Пример 1: 6+3=' + (6+3) + ' Ваш ответ '+ str);
+var str = prompt('Решите пример 20-5=?')
+console.log('пирмер 2: 20-5= '+ (20-5) + ' Ваш ответ '+str);
+var str = prompt('решите пример 15*2=?')
+console.log('Пример 3: 15*2= ' + 15*2 + ' Ваш ответ ' + str);
+var str = prompt('Решите пример 8/4=?')
+console.log('Пример 4: 8/2= ' + 8/4 + ' Ваш ответ ' + str);
+var str = prompt('Решите пример 10%3=?')
+console.log('Пример 5: 10%3= ' + 10%3 + ' Ваш ответ ' + str);
+var Name = 'Откройте консоль для просмотра'
+alert(Name)
